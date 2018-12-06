4c457844b9e253a1194704b5df7629c3c6c2e24a
Hristiyan Trifonov <hristiyantrifonov@Hristiyans-iMac.local>
2018-12-06 14:46:48 +0000

    Make app production ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # define the path to go to welcome controller and the home action of that controller
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
 
 end
