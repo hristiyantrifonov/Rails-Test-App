07ce101e742b6558fab6529c1b4095995e1cca7a
Hristiyan Trifonov <hristiyantrifonov@Hristiyans-iMac.local>
2018-12-07 12:32:22 +0000

    Enhancment of articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -10,8 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
-    puts params
   end
 
   # The create method for submission details go into the params hash
@@ -28,7 +27,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -38,12 +36,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
-
     if @article.destroy
       flash[:notice] = "Article was successfully deleterd"
       redirect_to articles_path
@@ -51,6 +46,9 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
